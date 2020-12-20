16a96e1a9e5f2c92ac5a99da395b002dc60c529a
master <58977579+alexmasterblack@users.noreply.github.com>
2020-12-20 18:32:18 +1000

    Добавьте файлы проекта.

---
+++ Xamarin_Notes/Xamarin_Notes/EditorPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin_Notes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditorPage : ContentPage
+    {
+        public DateTime NowTime;
+        public string NoteText { get; set; }
+        public EditorPage()
+        {
+            InitializeComponent();
+            NowTime = DateTime.Now;
+            DataSymbols.Text = NowTime.ToString();
+        }
+        public EditorPage(string text_note, DateTime now_time)
+        {
+            InitializeComponent();
+            NoteText = text_note;
+            FieldNote.Text = NoteText;
+            NowTime = now_time;
+            DataSymbols.Text = NowTime.ToString() + " | " + NoteText.Length.ToString();
+        }
+
+        private void Add_Clicked(object sender, EventArgs e)
+        {
+            NowTime = DateTime.Now;
+            NoteText = FieldNote.Text;
+            Navigation.PopAsync();
+        }
+    }
+}
---
+++ Xamarin_Notes/Xamarin_Notes/MainPage.xaml.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace Xamarin_Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        private List<Xamarin.Forms.Color> colors = new List<Xamarin.Forms.Color> { Color.Pink, Color.PapayaWhip, Color.Plum, Color.PaleTurquoise, Color.PaleGreen };
+        private LinkedList<Frame> notes = new LinkedList<Frame>();
+        private double left_height = 0;
+        private double right_height = 0;
+
+        private void LeftCount()
+        {
+            if (LeftNotes.Children.Count == 0 && RightNotes.Children.Count == 1)
+            {
+                var element = RightNotes.Children[RightNotes.Children.Count - 1];
+                RightNotes.Children.Remove(element);
+                LeftNotes.Children.Add(element);
+            }
+            if (LeftNotes.Children.Count - RightNotes.Children.Count >= 2)
+            {
+                var element = LeftNotes.Children[LeftNotes.Children.Count - 1];
+                LeftNotes.Children.Remove(element);
+                RightNotes.Children.Add(element);
+            }
+        }
+
+        private void RightCount()
+        {
+            if (LeftNotes.Children.Count == 0 && RightNotes.Children.Count == 1)
+            {
+                var element = RightNotes.Children[RightNotes.Children.Count - 1];
+                RightNotes.Children.Remove(element);
+                LeftNotes.Children.Add(element);
+            }
+            if (RightNotes.Children.Count - LeftNotes.Children.Count >= 2)
+            {
+                var element = RightNotes.Children[RightNotes.Children.Count - 1];
+                RightNotes.Children.Remove(element);
+                LeftNotes.Children.Add(element);
+            }
+        }
+
+        private void Count(IList<View> stack_one, IList<View> stack_two)
+        {
+            if (LeftNotes.Children.Count == 0 && RightNotes.Children.Count == 1)
+            {
+                var element = RightNotes.Children[RightNotes.Children.Count - 1];
+                RightNotes.Children.Remove(element);
+                LeftNotes.Children.Add(element);
+            }
+            if (stack_one.Count - stack_two.Count >= 2)
+            {
+                var element = stack_one[stack_one.Count - 1];
+                stack_one.Remove(element);
+                stack_two.Add(element);
+            }
+        }
+
+        private void OrderByDate()
+        {
+            foreach (Frame note in notes.OrderByDescending(elem => DateTime.Parse(((Label)((StackLayout)elem.Content).Children[1]).Text)))
+            {
+                if (left_height > right_height)
+                {
+                    RightNotes.Children.Add(note);
+                    right_height += note.Height;
+                }
+                else
+                {
+                    LeftNotes.Children.Add(note);
+                    left_height += note.Height;
+                }
+            }
+        }
+
+        private void SaveNotes()
+        {
+            var file_notes = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData), "saved_notes.json");
+            JArray left = new JArray();
+            JArray right = new JArray();
+            foreach(Frame note in LeftNotes.Children)
+            {
+                left.Add(new JObject()
+                {
+                    { "text", ((Label)((StackLayout)note.Content).Children[0]).Text },
+                    { "date", ((Label)((StackLayout)note.Content).Children[1]).Text }
+                });
+            }
+            foreach (Frame note in RightNotes.Children)
+            {
+                right.Add(new JObject()
+                {
+                    { "text", ((Label)((StackLayout)note.Content).Children[0]).Text },
+                    { "date", ((Label)((StackLayout)note.Content).Children[1]).Text }
+                });
+            }
+            JObject saved = new JObject()
+            {
+                { "left", left },
+                { "right", right }
+            };
+            File.WriteAllText(file_notes, saved.ToString());
+        }
+
+        private void AddNote(string text_note, DateTime now_time, string position = "")
+        {
+            Random rand = new Random();
+            var LabelText = new Label
+            {
+                Margin = new Thickness(0, 0, 0, 0),
+                LineBreakMode = LineBreakMode.WordWrap,
+                Text = text_note
+            };
+            var LabelInfo = new Label
+            {
+                Margin = new Thickness(0, 0, 0, 0),
+                LineBreakMode = LineBreakMode.WordWrap,
+                Text = now_time.ToString()
+            };
+            StackLayout NewNote = new StackLayout
+            {
+                Margin = new Thickness(0, 0, 0, 0),
+                Children = {
+                    LabelText,
+                    LabelInfo
+                }
+            };
+            Frame NoteFrame = new Frame
+            {
+                BackgroundColor = Color.PapayaWhip,
+                CornerRadius = 10,
+                Margin = new Thickness(10, 10, 10, 10),
+                Padding = new Thickness(15, 15, 15, 15),
+                Content = NewNote
+            };
+            //удаление по сдвигу
+            var pan = new PanGestureRecognizer();
+            double info = 0;
+            pan.PanUpdated += async (s, e) =>
+            {
+                switch (e.StatusType)
+                {
+                    case GestureStatus.Started:
+                        NoteFrame.TranslationX = 0;
+                        break;
+                    case GestureStatus.Running:
+                        if (e.TotalX > 0 && RightNotes.Children.Contains(NoteFrame))
+                        {
+                            info = e.TotalX;
+                        }
+                        else if (e.TotalX < 0 && LeftNotes.Children.Contains(NoteFrame))
+                        {
+                            info = e.TotalX;
+                        }
+                        break;
+                    case GestureStatus.Completed:
+                        if (info > 0 && RightNotes.Children.Contains(NoteFrame))
+                        {
+                            await NoteFrame.TranslateTo(NoteFrame.Width / 2, 0, 300, Easing.CubicInOut);
+                            if (await DisplayAlert("Delete", "Are you sure?", "Yes", "No"))
+                            {
+                                await NoteFrame.FadeTo(0, 1000, Easing.CubicInOut);
+                                RightNotes.Children.Remove(NoteFrame);
+                                Count(LeftNotes.Children, RightNotes.Children);
+                                //LeftCount();
+                                SaveNotes();
+                            } else
+                            {
+                                await NoteFrame.TranslateTo(0, 0, 300, Easing.CubicInOut);
+                            }
+                        }
+                        else if (info < 0 && LeftNotes.Children.Contains(NoteFrame))
+                        {
+                            await NoteFrame.TranslateTo(-NoteFrame.Width / 2, 0, 300, Easing.CubicInOut);
+                            if (await DisplayAlert("Delete", "Are you sure?", "Yes", "No"))
+                            {
+                                await NoteFrame.FadeTo(0, 1000, Easing.CubicInOut);
+                                LeftNotes.Children.Remove(NoteFrame);
+                                Count(RightNotes.Children, LeftNotes.Children);
+                                //RightCount();
+                                SaveNotes();
+                            } else
+                            {
+                                await NoteFrame.TranslateTo(0, 0, 300, Easing.CubicInOut);
+                            }
+                        }
+                        break;
+                }
+            };
+            NewNote.GestureRecognizers.Add(pan);
+            //касание по фрейму - апдейт заметки
+            var tap = new TapGestureRecognizer();
+            tap.Tapped += (sender, e) =>
+            {
+                EditorPage edit = new EditorPage(text_note, now_time);
+                edit.Disappearing += (send, ev) =>
+                {
+                    if (edit.NoteText.Length == 0)
+                    {
+                        if (RightNotes.Children.Contains(NoteFrame))
+                        {
+                            RightNotes.Children.Remove(NoteFrame);
+                            Count(LeftNotes.Children, RightNotes.Children);
+                            //LeftCount();
+                            SaveNotes();
+                        }
+                        if (LeftNotes.Children.Contains(NoteFrame))
+                        {
+                            LeftNotes.Children.Remove(NoteFrame);
+                            Count(RightNotes.Children, LeftNotes.Children);
+                            //RightCount();
+                            SaveNotes();
+                        }
+                        return;
+                    } else
+                    {
+                        text_note = edit.NoteText;
+                        LabelText.Text = text_note;
+                        now_time = edit.NowTime;
+                        LabelInfo.Text = now_time.ToString();
+                    }
+                };
+                if (Navigation.NavigationStack.Count == 1)
+                {
+                    Navigation.PushAsync(edit);
+                }
+            };
+            NewNote.GestureRecognizers.Add(tap);
+            
+            if (position == "")
+            {
+                if (LeftNotes.Height <= RightNotes.Height)
+                {
+                    LeftNotes.Children.Add(NoteFrame);
+                }
+                else
+                {
+                    RightNotes.Children.Add(NoteFrame);
+                }
+            } else
+            {
+                if (position == "left")
+                {
+                    LeftNotes.Children.Add(NoteFrame);
+                } else
+                {
+                    RightNotes.Children.Add(NoteFrame);
+                }
+            }
+           // notes.AddFirst(NoteFrame);
+           // if (position == "")
+           // {
+           //     if (RightNotes.Children.Count == 0 && LeftNotes.Children.Count == 0)
+            //    {
+            //        LeftNotes.Children.Add(NoteFrame);
+            //    } else
+           //     {
+           //         OrderByDate();
+          //      }
+          //  }
+           // else
+           // {
+           //     if (position == "left")
+           //     {
+           //         LeftNotes.Children.Add(NoteFrame);
+           //     }
+           //     else
+           //     {
+           //         RightNotes.Children.Add(NoteFrame);
+           //     }
+            //}
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+            var file_notes = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData), "saved_notes.json");
+            if (File.Exists(file_notes))
+            {
+                JObject notes = JObject.Parse(File.ReadAllText(file_notes));
+                foreach (JObject note in notes["right"])
+                {
+                    AddNote((string)note["text"], DateTime.Parse((string)note["date"]), "right");
+                }
+                foreach (JObject note in notes["left"])
+                {
+                    AddNote((string)note["text"], DateTime.Parse((string)note["date"]), "left");
+                }
+            }
+        }
+
+        private void Add_Clicked(object sender, EventArgs e)
+        {
+            var edit = new EditorPage();
+            edit.Disappearing += (send, ev) =>
+            {
+                if (edit.NoteText == null)
+                {
+                    return;
+                }
+                AddNote(edit.NoteText, edit.NowTime);
+                SaveNotes();
+            };
+            if (Navigation.NavigationStack.Count == 1)
+            {
+                Navigation.PushAsync(edit);
+            }
+        }
+
+        private void FindNote_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            foreach (Frame note in LeftNotes.Children)
+            {
+                Label text_note = (Label)((StackLayout)note.Content).Children[0];
+                if (text_note.Text.ToLower().Contains(FindNote.Text.ToLower()))
+                {
+                    note.IsVisible = true;
+                } else
+                {
+                    note.IsVisible = false;
+                }
+            }
+            foreach (Frame note in RightNotes.Children)
+            {
+                Label text_note = (Label)((StackLayout)note.Content).Children[0];
+                if (text_note.Text.ToLower().Contains(FindNote.Text.ToLower()))
+                {
+                    note.IsVisible = true;
+                }
+                else
+                {
+                    note.IsVisible = false;
+                }
+            }
+        }
+    }
+}
